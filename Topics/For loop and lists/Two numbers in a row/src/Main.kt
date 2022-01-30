f0d89fbe3e950cb9a32870a1ddd8b8cbc60e5563
Gehard <gehardnjoro@gmail.com>
2022-01-30 20:16:12 +0300

    track

--- Topics/For loop and lists/Two numbers in a row/src/Main.kt
+++ Topics/For loop and lists/Two numbers in a row/src/Main.kt
@@ -1,3 +1,5 @@
 fun main() {
-    // write your code here
+    val list = MutableList(readLine()!!.toInt()) { readLine()!! }.joinToString(" ")
+    val pattern = readLine()!!
+    println(if (list.contains(pattern) || list.contains(pattern.reversed())) "NO" else "YES")
 }
